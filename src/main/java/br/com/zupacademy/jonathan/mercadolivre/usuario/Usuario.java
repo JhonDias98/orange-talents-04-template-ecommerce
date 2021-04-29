0a64ccbd2cfeb6b3d6e2e8eae8e852512652ddc3
Jonathan Pereira Dias <52224050+JhonDias98@users.noreply.github.com>
2021-04-29 09:28:39 -0300

    Cadastro do usuário

---
+++ src/main/java/br/com/zupacademy/jonathan/mercadolivre/usuario/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.zupacademy.jonathan.mercadolivre.usuario;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	@Email
+	private String login;
+	@NotBlank
+	@Size(min = 6)
+	private String senha;
+	@NotNull
+	@CreationTimestamp
+	private LocalDateTime instanteCadastro = LocalDateTime.now();
+	
+	/**
+	 * 
+	 * @param login string em formato de email
+	 * @param senha string com texto limpo
+	 */
+	public Usuario(@NotBlank @Email String login, @NotNull @Valid SenhaLimpa senhaLimpa) {
+		Assert.isTrue(StringUtils.hasLength(login), "login não pode ser em branco");
+		Assert.notNull(senhaLimpa, "o objeto do tipo senha limpa não pode ser nulo");
+		this.login = login;
+		this.senha = senhaLimpa.hash();
+	}
+
+	@Deprecated
+	public Usuario() {}
+}
---
+++ src/main/java/br/com/zupacademy/jonathan/mercadolivre/usuario/UsuarioRequest.java
@@ -0,0 +1,25 @@
+package br.com.zupacademy.jonathan.mercadolivre.usuario;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UsuarioRequest {
+
+	@NotBlank
+	@Email
+	private String login;
+	@NotBlank
+	@Size(min = 6)
+	private String senha;
+	
+	public UsuarioRequest(@NotBlank @Email String login, @NotBlank @Size(min = 6) String senha) {
+		super();
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public Usuario toModel() {
+		return new Usuario(login, new SenhaLimpa(senha));
+	}
+}
